874d23abdf37e6605559e4dd00825f5703456549
Dave Glowacki <dglo@icecube.wisc.edu>
2007-05-09 11:46:44 -0500

    Issue 1392: Temporary workaround for global trigger bug

--- src/main/java/icecube/daq/eventBuilder/EventBuilderReadoutRequestGenerator.java
+++ src/main/java/icecube/daq/eventBuilder/EventBuilderReadoutRequestGenerator.java
@@ -301,6 +301,10 @@ public class EventBuilderReadoutRequestGenerator
                 generateStringGlobalRequest(eventReadoutRequests,
                                             inIceSources, eventId,
                                             firstTime, lastTime, timeStamp);
+                // XXX temporary hack to include AMANDA in in-ice triggers
+                generateStringGlobalRequest(eventReadoutRequests,
+                                            otherSources, eventId,
+                                            firstTime, lastTime, timeStamp);
                 break;
 
                 //-----------------------------------------------------
