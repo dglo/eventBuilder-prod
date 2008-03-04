2bf649fc9ffc1d554c9cb21b66bdbdddcc9ba09e
Keith Beattie <ksbeattie@lbl.gov>
2008-03-04 13:47:18 -0600

    Issue #2338: daq-io - output engine may be the cause of the IC40 failures
    Adding new MultiDestinationOutputEngine and changing
    EventBuilderSPreqPayloadOutputEngine to extend that rather than
    PayloadDestinationOutputEngine which in effect causes the EB to use
    MultiOutputEngine rather than PayloadOutputEngine.  Other components
    still use PayloadOutputEngine (which explains the change to
    DAQComponent making it aware of MultiOutputEngine).

--- src/main/java/icecube/daq/eventBuilder/EventBuilderSPreqPayloadOutputEngine.java
+++ src/main/java/icecube/daq/eventBuilder/EventBuilderSPreqPayloadOutputEngine.java
@@ -1,7 +1,7 @@
 /*
  * class: SystemTestPayloadOutputEngine
  *
- * Version $Id: EventBuilderSPreqPayloadOutputEngine.java 2125 2007-10-12 18:27:05Z ksb $
+ * Version $Id: EventBuilderSPreqPayloadOutputEngine.java 2751 2008-03-05 01:47:18Z ksb $
  *
  * Date: May 23 2005
  *
@@ -10,16 +10,16 @@
 
 package icecube.daq.eventBuilder;
 
-import icecube.daq.io.PayloadDestinationOutputEngine;
+import icecube.daq.io.MultiDestinationOutputEngine;
 
 /**
  * This class ...does what?
  *
  * @author mcp
- * @version $Id: EventBuilderSPreqPayloadOutputEngine.java 2125 2007-10-12 18:27:05Z ksb $
+ * @version $Id: EventBuilderSPreqPayloadOutputEngine.java 2751 2008-03-05 01:47:18Z ksb $
  */
 public class EventBuilderSPreqPayloadOutputEngine
-    extends PayloadDestinationOutputEngine
+    extends MultiDestinationOutputEngine
     implements RequestPayloadOutputEngine
 {
     /**
